d3a44dbf7cb8fd52211d6aabb099972126928251
shuchitasingh <singhshuchita5@gmail.com>
2021-04-13 12:44:57 +0530

    Added: Enter first name

---
+++ FirstName.java
@@ -0,0 +1,20 @@
+package user.register;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+
+public class FirstName {
+
+	public void checkPatternFirstName() {
+		String firstNamePattern = "^[A-Z]{1}[a-z]{2,}$";
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("enter the First Name: ");
+		String checkPattern = userInput.next();
+
+		if (Pattern.matches(firstNamePattern, checkPattern))
+			System.out.println("Pattern match.");
+		else
+			System.out.println("patern match fail! please try again!!!");
+	}
+}
---
+++ Main.java
@@ -0,0 +1,19 @@
+package user.register;
+
+import java. util.regex.*;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Welcome to user registration file.");
+		
+		FirstName firstName = new FirstName();
+		LastName lastName =new LastName();
+		
+		
+		
+		firstName.checkPatternFirstName();
+		lastName.checkPatternLastName();
+	}
+	
+
+}
