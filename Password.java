5f8ff658d35713a106b93fdaf73bfe9c81e4dbe8
shuchitasingh <singhshuchita5@gmail.com>
2021-04-13 13:40:34 +0530

    Added:should have at least one numeric number

--- Password.java
+++ Password.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class Password {
 	public void checkPatternPassword() {
-		String passwordPattern = "^[\\d[a-z]*(?=.*[A-Z]).{8,}$";
+		String passwordPattern =  "^(?=.*\\d)([a-z])*(?=.*[A-Z]).{8,}$";
 		Scanner userInput = new Scanner(System.in);
 		System.out.println("enter the Password: ");
 		String checkPattern = userInput.next();
